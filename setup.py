373caec97b7d6b7cad7479cfc98ab347e752e6d4
IdoAtNP <ido@nplabs.ai>
2025-10-24 02:06:15 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
-    name="terraform-resource-documentation",
+    name="terraform_resource_documentation",
     version="0.1.0",
     author="NPLabs",
     description="Extract and format documentation from Terraform provider resources",
@@ -29,11 +29,6 @@ setup(
         "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.7",
-    install_requires=requirements,
-    entry_points={
-        "console_scripts": [
-            "terraform-resource-docs=terraform_doc_extractor.cli:main",
-        ],
-    },
+    install_requires=requirements
 )
 
